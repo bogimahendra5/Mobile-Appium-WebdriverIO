5c2e9e080675836c32e51dcc274608bf6b99be4c
bogimahendra5 <bogimahendra5@gmail.com>
2024-05-11 19:28:51 +0700

    Add reporter Allure

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,4 @@
+import allure from "allure-commandline";
 //import projectPath from "path";
 //const androidAppPath = projectPath.join(process.cwd(), "app/android/Android-MyDemoAppRN.1.1.0.build-266.apk");
 
@@ -113,7 +114,14 @@ export const config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ["appium"],
+  services: [
+    [
+      "appium",
+      {
+        command: "appium",
+      },
+    ],
+  ],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
@@ -136,7 +144,36 @@ export const config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+      },
+    ],
+  ],
+
+  onComplete: function () {
+    const reportError = new Error("Could not generate Allure report");
+    const generation = allure(["generate", "allure-results", "--clean"]);
+    return new Promise((resolve, reject) => {
+      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+
+      generation.on("exit", function (exitCode) {
+        clearTimeout(generationTimeout);
+
+        if (exitCode !== 0) {
+          return reject(reportError);
+        }
+
+        console.log("Allure report successfully generated");
+        resolve();
+      });
+    });
+  },
 
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
